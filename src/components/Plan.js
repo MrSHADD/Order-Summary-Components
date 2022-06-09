3c79ade75d780b95e08cdfca0384f84baa07f9a2
Israel Oluwafemi Alabi <israelalabi97@yahoo.com>
2022-06-09 01:28:57 +0100

    Update Plan.js

--- src/components/Plan.js
+++ src/components/Plan.js
@@ -2,15 +2,13 @@ import React from "react"
 
 export default function Plan(){
     return(
-       <div>
-        <plan>
+       <div className="plan">
            <img src="./images/icon-music.svg" className="music--img" alt="music icon"/>
            <div className="div--price">
                <h2>Annual Plan <br></br><span class= "pricee">$59.99/year</span></h2>
                {/* <h2>$59.99/year</h2> */}
            </div>
            <a className="change" href="#">Change</a>
-        </plan>
        </div>
     )
 }
