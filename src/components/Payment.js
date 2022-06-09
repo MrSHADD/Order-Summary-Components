f08ba125bb717dc08892021266bd8eecdbe843ed
Israel Oluwafemi Alabi <israelalabi97@yahoo.com>
2022-06-09 01:33:42 +0100

    Update Payment.js

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -2,13 +2,11 @@ import React from "react";
 
 export default function Hero(){
     return(
-        <div>
-        <pay>
+        <div className="pay">
            <button>Proceed to Payment</button>
            <h3>
            <a href="#" className="cancel">Cancel Order</a>
            </h3>
-        </pay>
         </div>
     )
 }
