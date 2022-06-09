b251e4a5100a58ae025f46bc3455e656e89c4cb6
Israel Oluwafemi Alabi <israelalabi97@yahoo.com>
2022-06-09 01:31:10 +0100

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,14 +2,12 @@ import React from "react";
 
 export default function Hero(){
     return(
-        <div>
-        <hero>
+        <div className="hero">
            <img src="./images/illustration-hero.svg" className="hero--img" alt="guy enjoying music"/>
            <h1>Order Summary</h1>
            <p> You can now listen to millions of songs, audiobooks, 
                and podcasts on any  device anywhere you like!
             </p>
-        </hero>
         </div>
     )
 }
